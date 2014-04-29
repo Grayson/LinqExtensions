6f0c5b4bb274d60d2f2ee35cf3c01475a9da7ea1
Grayson <info@fromconcentratesoftware.com>
2014-04-29 06:51:48 -0700

    Initial Commit

---
+++ LinqExtensions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LinqExtensions
+//
+//  Created by Grayson Hansard on 4/29/14.
+//  Copyright (c) 2014 From Concentrate Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
