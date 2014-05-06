d7ac9627e7ab610b07315a98ed6856a8cf692805
Grayson <info@fromconcentratesoftware.com>
2014-05-05 21:10:07 -0700

    Minor Xcode project management

--- LinqExtensions/main.m
+++ LinqExtensions/main.m
@@ -14,45 +14,7 @@ int main(int argc, const char * argv[])
 {
 
 	@autoreleasepool {
-		TestObject *t1 = [TestObject new];
-		TestObject *t2 = [TestObject new];
-		TestObject *t3 = [TestObject new];
-		
-		t1.content = @(1);
-		t2.content = @(2);
-		t3.content = @(3);
-		
-	    NSArray *array = @[t1, t2, t3];
-		NSEnumerator *enumerator = array.select(^(TestObject *o) { return o.content; });
-		for (NSNumber *number in enumerator)
-			NSLog(@"%@", number);
-		
-		NSLog(@"--");
-		
-		enumerator = array.where(^(TestObject *o) { return [o.content intValue] > 1; });
-		for (NSNumber *number in enumerator)
-			NSLog(@"%@", number);
-		
-		NSLog(@"--");
-		enumerator = array.where(^(TestObject *o) { return [o.content intValue] > 100; });
-		for (NSNumber *number in enumerator)
-			NSLog(@"%@", number);
-		
-		NSLog(@"--");
-		BOOL any = array.any(^(TestObject *o) { return [o.content intValue] == 1; });
-		NSLog(@"%@matches", any ? @"" : @"no ");
-
-		any = array.any(^(TestObject *o) { return [o.content intValue] == 100; });
-		NSLog(@"%@matches", any ? @"" : @"no ");
-		
-		NSLog(@"--");
-		BOOL all = array.all(^(TestObject *o) { return [o.content intValue] < 100; });
-		NSLog(@"all: %u", all);
-		
-		all = array.all(^(TestObject *o) { return [o.content intValue] != 2; });
-		NSLog(@"all: %u", all);
-		
-}
+	}
     return 0;
 }
 
